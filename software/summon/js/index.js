6b7bb6e349d4d42b75fef48a7e85eee6b3568ac6
Cole Hudson <cmhudson11@gmail.com>
2016-06-29 17:09:09 -0400

    rev19: string to array buffer maybe working now

--- software/summon/js/index.js
+++ software/summon/js/index.js
@@ -20,6 +20,18 @@ $(document).on('pageinit',function(){
     });
 });
 
+
+var str2ab = function(str) 
+{
+    var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
+    var bufView = new Uint16Array(buf);
+    for (var i=0, strLen=str.length; i<strLen; i++) {
+        bufView[i] = str.charCodeAt(i);
+    }
+    return buf;
+}
+
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -70,7 +82,7 @@ var app = {
                 console.log("CONNECTION SUCCESSFUL");
 
                 //convert text to format for write
-                var buffer = bluetooth.str2ab(writeValue);
+                var buffer = str2ab(writeValue);
 
                 bluetooth.write(deviceId, serviceUuid, characteristicUuid, buffer);
 
