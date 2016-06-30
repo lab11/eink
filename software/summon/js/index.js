1191cbf3e5a57b9e7d5cd4a8392f6033f611205b
Cole Hudson <cmhudson11@gmail.com>
2016-06-29 21:00:22 -0400

    rev30: stringtobytes

--- software/summon/js/index.js
+++ software/summon/js/index.js
@@ -21,14 +21,12 @@ $(document).on('pageinit',function(){
 });
 
 
-var str2ab = function(str) 
-{
-    var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
-    var bufView = new Uint16Array(buf);
-    for (var i=0, strLen=str.length; i<strLen; i++) {
-        bufView[i] = str.charCodeAt(i);
+function stringToBytes(string) {
+    var array = new Uint8Array(string.length);
+    for (var i = 0, l = string.length; i < l; i++) {
+        array[i] = string.charCodeAt(i);
     }
-    return buf;
+    return array.buffer;
 }
 
 
