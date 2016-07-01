26a759d039df106756e73f6b3ec29d2c87c91830
Cole Hudson <cmhudson11@gmail.com>
2016-06-30 21:38:24 -0400

    rev49: modified stringify

--- software/summon/js/index.js
+++ software/summon/js/index.js
@@ -97,9 +97,9 @@ function scanConnectWrite(charUuid, buffer, callback)
         bluetooth.startScan([], function(device){
             if(device.id == deviceId)
             {
-                console.log(JSON.stringify(device));
+                console.log(JSON.stringify(device, null, 2));
 
-                
+
                 writeConnect(charUuid, buffer, device, callback);
             }
         }, function(error){
